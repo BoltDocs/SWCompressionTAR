2299ba03cbc01b552a4d3a833b0ad812c16dd277
Ethan Wong <e1hanw0ng@gmail.com>
2022-12-31 18:27:42 +0800

    Commit for SWCompressionTAR.

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "SWCompression",
+    name: "SWCompressionTAR",
     platforms: [
         .macOS(.v10_13),
         .iOS(.v11),
@@ -13,29 +13,20 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "SWCompression",
-            targets: ["SWCompression"]),
+            name: "SWCompressionTAR",
+            targets: ["SWCompressionTAR"]),
     ],
     dependencies: [
-        .package(name: "BitByteData", url: "https://github.com/tsolomko/BitByteData",
-                 from: "2.0.0"),
-        .package(name: "SwiftCLI", url: "https://github.com/jakeheis/SwiftCLI",
-                 from: "6.0.0"),
+        .package(name: "BitByteData", url: "https://github.com/tsolomko/BitByteData.git",
+                 .exact("2.0.4")),
     ],
     targets: [
         .target(
-            name: "SWCompression",
+            name: "SWCompressionTAR",
             dependencies: ["BitByteData"],
             path: "Sources",
-            exclude: ["swcomp"],
-            sources: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib"],
+            sources: ["Common", "TAR"],
             resources: [.copy("PrivacyInfo.xcprivacy")]),
-        .target(
-            name: "swcomp",
-            dependencies: ["SWCompression", "SwiftCLI"],
-            path: "Sources",
-            exclude: ["Common", "7-Zip", "BZip2", "Deflate", "GZip", "LZ4", "LZMA", "LZMA2", "TAR", "XZ", "ZIP", "Zlib", "PrivacyInfo.xcprivacy"],
-            sources: ["swcomp"]),
     ],
     swiftLanguageVersions: [.v5]
 )
